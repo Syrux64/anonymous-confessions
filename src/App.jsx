df4f5cb94b36e3d8788ba57e1c87906475846b2b
Syrux64 <mohandasssurya22@gmail.com>
2024-10-09 18:38:01 +0530

    added button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {formRef} from 'react'
 import './App.css'
 
 const App = () => {
@@ -7,6 +7,14 @@ const App = () => {
       <div className='title'>
         <h1>Anonymous Confession </h1>
       </div>
+      <form name='student-form'>
+        <div>
+          <textarea cols={60} rows={20}></textarea>
+        </div>
+        <div className='submit-buton'>
+          <input type="submit" value="Confess" id="submit" />
+        </div>
+      </form>
     </>
     
 
